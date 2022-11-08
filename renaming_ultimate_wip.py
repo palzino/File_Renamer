82f7cfd201d830f4ac6cb3f633e13b9a605188b2
Palvir <59867306+palzino@users.noreply.github.com>
2022-11-08 01:13:04 +0000

    The basis for the automated renaming

--- renaming_ultimate_wip.py
+++ renaming_ultimate_wip.py
@@ -1,24 +1,17 @@
 import os
-while True:
-    selection = input("input 1 to run the program and 2 to quit")
-    if selection == "1":
-        series_name = input("Input the name of the series")
-        season_number = input("input season number")
+
+x = 1
+working_dir = os.getcwd()
+split = os.path.split(working_dir)
+print(split[0], "break",split[1])
+season = (f"Season {x}")
+for f in os.listdir():
+    if f == season:
         f_ep = 1
-        mylist = []
-        working_dir = input("input directory")
-        for f in os.listdir():
-            file_name, file_ext = os.path.splitext(f)
-            if file_ext == ".py":
-                continue
-            else:
-                ep_format = (f"S{season_number}ep{f_ep}")
-                f_ep = f_ep + 1
-                new_name = ('{}{}{}'.format(series_name + " ", ep_format, file_ext))
-                os.rename(f, new_name)
-    elif selection == "2":
-        break
-    else:
-        print("enter a valid response")
+        os.chdir(os.path.join(working_dir, season))
+        for x in os.listdir():
+            print(x)
             
-       
+        x = + 1
+    else:
+        print("not here")
