2cc3f8298f62680f58bbc97545480137a057fdbe
Palvir <59867306+palzino@users.noreply.github.com>
2023-04-07 02:17:36 +0100

    Update rename-script.py

--- rename-script.py
+++ rename-script.py
@@ -13,6 +13,6 @@ for f in os.listdir():
             else:
                 ep_format = (f"S{s:02d} EP{x_ep:02d}")
                 x_ep = x_ep + 1
-                os.rename = (x, '{}{}{}'.format(series_name + " ", ep_format, file_ext))
+                os.rename (x, '{}{}{}'.format(series_name + " ", ep_format, file_ext))
         os.chdir(os.path.pardir)
         s = s + 1
